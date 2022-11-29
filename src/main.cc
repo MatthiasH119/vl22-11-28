95c6ce93c0ccda1e12d3efa55c042f7902ee6c1e
MatthiasH119 <matthiasheiming@aol.de>
2022-11-29 21:51:03 +0100

    add CLI Fetch

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
 
-int main (){
+int main (int argc, char** argv){
+    CLI::App app{"1","2"};
+    try{
+        app.parse(argc, argv);
+    } catch(const CLI::ParseError& e){
+        return app.exit(e);
+    }
     std::cout << "Hello" << std::endl;
 }
